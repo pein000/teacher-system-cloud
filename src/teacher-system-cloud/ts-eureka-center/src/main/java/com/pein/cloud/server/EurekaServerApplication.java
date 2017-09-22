667f3ea3438398b404d0e4ce39a61c8eadfc4a90
qiuw <qiuw@2345.com>
2017-09-22 19:53:41 +0800

    添加jar -jar --spring.profiles.active注释

--- src/teacher-system-cloud/ts-eureka-center/src/main/java/com/pein/cloud/server/EurekaServerApplication.java
+++ src/teacher-system-cloud/ts-eureka-center/src/main/java/com/pein/cloud/server/EurekaServerApplication.java
@@ -6,6 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by pein on 2017/6/3.
+ *
+ * java -jar --spring.profiles.active=nodeA
+ * java -jar --spring.profiles.active=nodeB
+ * java -jar --spring.profiles.active=nodeC
  */
 @EnableEurekaServer
 @SpringBootApplication
